35dc7f51931d7a218fed99da4656ecb13d669bc0
lucaas-d3v <lucas.workps@gmail.com>
2026-01-21 16:13:01 -0300

    fix: github actions adicionado

---
+++ src/pages/download.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+export default function Download(): JSX.Element {
+    return (
+        <Layout title="Download">
+            <main style={{ padding: "2rem" }}>
+                <h1>Download</h1>
+                <p>OK</p>
+            </main>
+        </Layout>
+    );
+}
